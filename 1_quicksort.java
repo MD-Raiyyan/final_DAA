bf5fa0f9be19a7b09482337a2bae52776ca6de6c
Mohammed Raiyyan Bepari <mohammedraiyyanbepari@gmail.com>
2026-06-13 18:45:27 +0530

    Refactor QuickSort implementation and add swap method
    Refactored quickSort and partition methods for clarity and efficiency. Added swap method to improve code modularity.

--- 1_quicksort.java
+++ 1_quicksort.java
@@ -2,6 +2,17 @@ import java.util.Scanner;
 
 class QuickSort {
 
+    static void quickSort(int arr[], int low, int high) {
+
+        if (low < high) {
+
+            int p = partition(arr, low, high);
+
+            quickSort(arr, low, p - 1);
+            quickSort(arr, p + 1, high);
+        }
+    }
+
     static int partition(int arr[], int low, int high) {
 
         int pivot = arr[low];
@@ -13,33 +24,25 @@ class QuickSort {
             while (i <= high && arr[i] < pivot)
                 i++;
 
-            while (arr[j] > pivot)
+            while (j >= low && arr[j] > pivot)
                 j--;
 
-            if (i >= j)
+            if (i < j)
+                swap(arr, i, j);
+            else
                 break;
-
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
         }
 
-        int temp = arr[low];
-        arr[low] = arr[j];
-        arr[j] = temp;
+        swap(arr, low, j);
 
         return j;
     }
 
-    static void quickSort(int arr[], int low, int high) {
+    static void swap(int arr[], int i, int j) {
 
-        if (low < high) {
-
-            int p = partition(arr, low, high);
-
-            quickSort(arr, low, p - 1);
-            quickSort(arr, p + 1, high);
-        }
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     public static void main(String args[]) {
@@ -56,13 +59,18 @@ class QuickSort {
         for (int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
 
+        System.out.println("Original Array:");
+
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+
         long start = System.nanoTime();
 
         quickSort(arr, 0, n - 1);
 
         long end = System.nanoTime();
 
-        System.out.println("Sorted Array:");
+        System.out.println("\nSorted Array:");
 
         for (int i = 0; i < n; i++)
             System.out.print(arr[i] + " ");
